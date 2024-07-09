dfb8b7adc4eb607c0df9f47c39aca72835166de4
chen <chen.ling@artifacture.co.jp>
2024-07-09 15:59:18 +0900

    feat: create basic line chart with responsive compatible

--- src/Pages/LineChartPage.tsx
+++ src/Pages/LineChartPage.tsx
@@ -1,34 +1,125 @@
-import { Typography } from "@mui/material";
+import { css } from "@emotion/react";
+import { Box, Typography } from "@mui/material";
 import { FC } from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 import AppBar from "../Components/AppBar";
+import DateRangePicker from "../Components/DateRangePicker";
+import useMedia from "../Hooks/useMedia";
+import { defaultTheme } from "../theme/default";
 
-const LineChartPage: FC = () => (
-  <AppBar>
-    <Typography variant="h3">Line Chart Page</Typography>
-    <Typography paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo
-      vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque
-      non tellus. Convallis convallis tellus id interdum velit laoreet id donec ultrices. Odio morbi
-      quis commodo odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-      integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate eu scelerisque felis
-      imperdiet proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-      lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis at consectetur lorem.
-      Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-    </Typography>
-    <Typography paragraph>
-      Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-      facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt.
-      Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-      mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed vulputate
-      odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-      gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et tortor.
-      Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-      Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas accumsan lacus
-      vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-    </Typography>
-  </AppBar>
-);
+const dummyData = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const styles = {
+  graphBox: css`
+    height: 100%;
+    width: 100%;
+    padding: 1.5rem 0;
+    display: flex;
+    flex-direction: column;
+    > * + * {
+      margin-top: 0.5rem;
+    }
+  `,
+};
+
+interface LineChartPageProps {
+  isWideScreen: boolean;
+}
+
+const LineChartPage: FC<LineChartPageProps> = () => {
+  const { isWideScreen } = useMedia();
+
+  return (
+    <AppBar>
+      <DateRangePicker />
+      <Box css={styles.graphBox}>
+        <Typography
+          alignSelf="center"
+          color={defaultTheme.palette.text.secondary}
+          variant={isWideScreen ? "body1" : "body2"}
+        >
+          東京都新型コロナコールセンター相談件数
+        </Typography>
+        <ResponsiveContainer
+          width={isWideScreen ? "95%" : "100%"}
+          height={isWideScreen ? "80%" : "50%"}
+        >
+          <LineChart
+            width={1000}
+            height={500}
+            data={dummyData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      </Box>
+    </AppBar>
+  );
+};
 
 export default LineChartPage;
