12e097c293a59e7000b669edaa76f01b09932f0f
chen <chen.ling@artifacture.co.jp>
2024-07-10 13:52:43 +0900

    feat: modify range picker to specific year/month picker

--- src/Components/DateRangePicker.tsx
+++ src/Components/DateRangePicker.tsx
@@ -1,15 +1,13 @@
-import { css, Theme } from "@emotion/react";
-import { FormControl, InputLabel, Select, MenuItem, Box, Typography } from "@mui/material";
+import { css } from "@emotion/react";
+import { FormControl, InputLabel, Select, MenuItem, Box } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
-import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 import { FC, useEffect, useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "../Hooks/reduxHooks";
 import useMedia from "../Hooks/useMedia";
-import { setEndDate, setStartDate } from "../redux-modules/dateRangePickerSlice";
+import { setEndDate, setStartDate } from "../redux-modules/datePickerSlice";
 import { setMode } from "../redux-modules/displayModeSlice";
-import { defaultTheme } from "../theme/default";
-import { yearList } from "../types/date";
+import { maxDate, minDate, yearList } from "../types/date";
 import { Mode, displayMode } from "../types/display-mode";
 
 const styles = {
@@ -21,55 +19,35 @@ const styles = {
       margin-top: ${isWideScreen ? 0 : "1rem"};
     }
   `,
-  selectRangeArea: css`
-    height: 4rem;
-  `,
-  selectRangeBox: css`
-    display: flex;
-    flex-direction: row;
-  `,
   modeSelect: css`
     width: 13rem;
-  `,
-  yearSelect: css`
-    width: 6.5rem;
   `,
   yearMonthSelect: css`
     width: 8rem;
   `,
-  rangeErrorHelpText: (theme: Theme) => css`
-    color: ${theme.palette.error.main};
-  `,
 };
 
 interface YearPickerProps {
-  id: string;
-  label: string;
-  dateValue: Date;
-  setDateValue: ActionCreatorWithPayload<
-    Date,
-    "dateRangePicker/setStartDate" | "dateRangePicker/setEndDate"
-  >;
-  isError: boolean;
+  startDate: Date;
 }
-const YearPicker: FC<YearPickerProps> = ({ id, label, dateValue, setDateValue, isError }) => {
+const YearPicker: FC<YearPickerProps> = ({ startDate }) => {
+  const [yearValue, setYearValue] = useState<number>(startDate.getFullYear());
   const dispatch = useAppDispatch();
-  const [yearValue, setYearValue] = useState<number>(dateValue.getFullYear());
 
   useEffect(() => {
-    dispatch(setDateValue(new Date(yearValue, 0, 1)));
-  }, [dispatch, setDateValue, yearValue]);
+    dispatch(setStartDate(new Date(yearValue, 0, 1)));
+    dispatch(setEndDate(new Date(yearValue, 11, 31)));
+  }, [dispatch, yearValue]);
 
   return (
     <FormControl>
-      <InputLabel id={id}>{label}</InputLabel>
+      <InputLabel id="year-select">年</InputLabel>
       <Select
-        labelId={id}
-        id={id}
+        labelId="year-select"
+        id="year-select"
         value={yearValue}
-        label={label}
+        label="年"
         onChange={(e) => setYearValue(Number(e.target.value))}
-        error={isError}
       >
         {yearList.map((year) => (
           <MenuItem key={year} value={year}>
@@ -82,39 +60,27 @@ const YearPicker: FC<YearPickerProps> = ({ id, label, dateValue, setDateValue, i
 };
 
 interface YearMonthPickerProps {
-  label: string;
-  dateValue: Date;
-  setDateValue: ActionCreatorWithPayload<
-    Date,
-    "dateRangePicker/setStartDate" | "dateRangePicker/setEndDate"
-  >;
-  isError: boolean;
+  startDate: Date;
 }
-const YearMonthPicker: FC<YearMonthPickerProps> = ({ label, dateValue, setDateValue, isError }) => {
-  const { startDate, endDate } = useAppSelector((state) => state.dateRangePicker);
+const YearMonthPicker: FC<YearMonthPickerProps> = ({ startDate }) => {
+  const [dateValue, setDateValue] = useState<Date>(startDate);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setStartDate(new Date(dateValue.getFullYear(), dateValue.getMonth(), 1)));
+    dispatch(setEndDate(new Date(dateValue.getFullYear(), dateValue.getMonth() + 1, 0)));
+  }, [dateValue, dispatch]);
 
   return (
     <DatePicker
-      minDate={startDate}
-      maxDate={endDate}
-      label={label}
+      minDate={minDate}
+      maxDate={maxDate}
+      label="年/月"
       views={["year", "month"]}
       format="yyyy/MM"
       css={styles.yearMonthSelect}
       value={dateValue}
-      onChange={(d) => d && dispatch(setDateValue(d))}
-      sx={{
-        "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-          border: `1px solid ${isError ? defaultTheme.palette.error.main : defaultTheme.palette.grey[400]}`,
-        }, // at page load
-        "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
-          border: `1px solid ${isError ? defaultTheme.palette.error.main : defaultTheme.palette.grey[900]}`,
-        }, // at hover state
-        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-          border: `2px solid ${isError ? defaultTheme.palette.error.main : defaultTheme.palette.primary.main}`,
-        }, // at focused state
-      }}
+      onAccept={(d) => d && setDateValue(d)}
     />
   );
 };
@@ -122,23 +88,8 @@ const YearMonthPicker: FC<YearMonthPickerProps> = ({ label, dateValue, setDateVa
 const DateRangePicker: FC = () => {
   const { isWideScreen } = useMedia();
   const mode = useAppSelector((state) => state.displayMode.mode);
-  const { startDate, endDate } = useAppSelector((state) => state.dateRangePicker);
+  const { startDate } = useAppSelector((state) => state.datePicker);
   const dispatch = useAppDispatch();
-  const [isRangeError, setIsRangeError] = useState<boolean>(false);
-
-  console.log({
-    mode,
-    startDate,
-    endDate,
-  });
-
-  useEffect(() => {
-    if (startDate > endDate) {
-      setIsRangeError(true);
-    } else {
-      setIsRangeError(false);
-    }
-  }, [startDate, endDate]);
 
   return (
     <Box css={() => styles.selectArea(isWideScreen)}>
@@ -161,47 +112,10 @@ const DateRangePicker: FC = () => {
           </Select>
         </FormControl>
       </Box>
-      <Box css={styles.selectRangeArea}>
-        <Box css={styles.selectRangeBox}>
-          {mode === "YEAR" && (
-            <>
-              <YearPicker
-                id="start-year"
-                label="開始年"
-                dateValue={startDate}
-                setDateValue={setStartDate}
-                isError={isRangeError}
-              />
-              <YearPicker
-                id="end-year"
-                label="終了年"
-                dateValue={endDate}
-                setDateValue={setEndDate}
-                isError={isRangeError}
-              />
-            </>
-          )}
-          {(mode === "YEAR_MONTH" || mode === "YEAR_MONTH_DAY") && (
-            <>
-              <YearMonthPicker
-                label="開始年月"
-                dateValue={startDate}
-                setDateValue={setStartDate}
-                isError={isRangeError}
-              />
-              <YearMonthPicker
-                label="終了年月"
-                dateValue={endDate}
-                setDateValue={setEndDate}
-                isError={isRangeError}
-              />
-            </>
-          )}
-        </Box>
-        {isRangeError && (
-          <Typography variant="caption" css={styles.rangeErrorHelpText}>
-            開始年月は終了年月より前の日を入力してください
-          </Typography>
+      <Box>
+        {mode === "YEAR" && <YearPicker startDate={startDate} />}
+        {(mode === "YEAR_MONTH" || mode === "YEAR_MONTH_DAY") && (
+          <YearMonthPicker startDate={startDate} />
         )}
       </Box>
     </Box>
